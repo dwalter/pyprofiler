57f336ef8e83b426812f2571286ba28b08396f62
dwalter <dpwalter7@gmail.com>
2020-12-17 14:04:21 -0500

    first commit

---
+++ pyprofiler/__init__.py
@@ -0,0 +1,32 @@
+import cProfile
+import pstats
+from functools import wraps
+
+
+def pyprofiler(
+    output_file=None, sort_by="time", lines_to_print=None, strip_dirs=True
+):
+    def inner(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            _output_file = output_file or func.__name__ + ".prof"
+            pr = cProfile.Profile()
+            pr.enable()
+            retval = func(*args, **kwargs)
+            pr.disable()
+            pr.dump_stats(_output_file)
+
+            with open(_output_file, "w") as f:
+                ps = pstats.Stats(pr, stream=f)
+                if strip_dirs:
+                    ps.strip_dirs()
+                if isinstance(sort_by, (tuple, list)):
+                    ps.sort_stats(*sort_by)
+                else:
+                    ps.sort_stats(sort_by)
+                ps.print_stats(lines_to_print)
+            return retval
+
+        return wrapper
+
+    return inner
---
+++ setup.py
@@ -0,0 +1,12 @@
+# from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="pyprofiler",
+    version="0.1",
+    description="Decorator for runtime profiling.",
+    url="https://github.com/dwalter/pyprofiler.git",
+    packages=["pyprofiler"],
+    # install_requires=[],
+    python_requires=">=3.6",
+)
